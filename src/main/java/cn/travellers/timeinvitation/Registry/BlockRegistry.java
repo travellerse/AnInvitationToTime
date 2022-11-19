a35d3f8415e654e2e98716bb6e8ca4fabeb3ad4f
a1394332633 <1394332633@qq.com>
2022-11-19 18:45:03 +0800

    修正

--- src/main/java/cn/travellers/timeinvitation/Registry/BlockRegistry.java
+++ src/main/java/cn/travellers/timeinvitation/Registry/BlockRegistry.java
@@ -5,7 +5,9 @@ import cn.travellers.timeinvitation.Block.OldVinesTopBlock;
 import cn.travellers.timeinvitation.Utils;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.EntityType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,13 +16,19 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Utils.MOD_ID);
     public static final Block oldVines = register("old_vines",new OldVinesTopBlock(AbstractBlock.Properties.create(Material.PLANTS).tickRandomly().doesNotBlockMovement().zeroHardnessAndResistance().sound(SoundType.NETHER_VINE)));
-    public static final Block oldVinesPlant = register("old_vines_plant",new OldVinesBlock(AbstractBlock.Properties.create(Material.PLANTS).tickRandomly().doesNotBlockMovement().zeroHardnessAndResistance().sound(SoundType.NETHER_VINE)));
-    public static final Block fernStem = register("fern_stem",new Block(AbstractBlock.Properties.create(Material.WOOD).hardnessAndResistance(2.0F, 3.0F).sound(SoundType.WOOD)));
+    public static final Block oldVinesPlant = register("old_vines_plant",new OldVinesBlock(AbstractBlock.Properties.create(Material.PLANTS).doesNotBlockMovement().zeroHardnessAndResistance().sound(SoundType.NETHER_VINE)));
+    public static final Block fernStem = register("fern_stem",createLogBlock(MaterialColor.WOOD, MaterialColor.OBSIDIAN));
     public static final Block fernLeaves = register("fern_leaves",createLeavesBlock());
 
     public static <T extends Block> T register(String name,T block){
         BLOCKS.register(name,()->block);
         return block;
+    }
+
+    private static RotatedPillarBlock createLogBlock(MaterialColor topColor, MaterialColor barkColor) {
+        return new RotatedPillarBlock(AbstractBlock.Properties.create(Material.WOOD, (state) -> {
+            return state.get(RotatedPillarBlock.AXIS) == Direction.Axis.Y ? topColor : barkColor;
+        }).hardnessAndResistance(2.0F).sound(SoundType.WOOD));
     }
 
     private static LeavesBlock createLeavesBlock() {
